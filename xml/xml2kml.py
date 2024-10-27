1673abeed6dbdbe2a9dcd084d928a91aec807f41
UO289510 <UO289510@uniovi.es>
2024-10-27 17:43:09 +0100

    Add files via upload

--- xml/xml2kml.py
+++ xml/xml2kml.py
@@ -1,25 +1,185 @@
 import xml.etree.ElementTree as ET
+import re
 
-def verXPath(archivoXML, expresionXPath):
-    try:
-        arbol = ET.parse(archivoXML)
-    except IOError:
-        print('No se encuentra el archivo ', archivoXML)
-        exit()
-    except ET.parseError:
-        print("Error procesando en el archivo XML = ", archivoXML)
-        exit()
+class Kml(object):
 
-    raiz = arbol.getroot()
+    def __init__(self):
+        self.raiz = ET.Element('kml', xmlns="http://www.opengis.net/kml/2.2")
+        self.doc = ET.SubElement(self.raiz, 'Document')
+        self.coordenadas = ""
+        self.salida = ""
 
-    for hijo in raiz.findall(expresionXPath):
-        print("\nElemento = ", hijo.tag)
-        if hijo.text != None:
-            print("Contenido = ", hijo.text.strip('\n'))
+
+    def addTramo(self,numero, sector, latitud, longitud, altitud, modoAltitud):
+        pm = ET.SubElement(self.doc, 'Tramo')
+        ET.SubElement(pm,'numero').text = '\n' + numero + '\n'
+        ET.SubElement(pm,'sector').text = '\n' + sector + '\n'
+        punto = ET.SubElement(pm,'Punto')
+        ET.SubElement(punto,'coordenadas').text = '\n{},{},{}\n'.format(longitud,latitud,altitud)
+        ET.SubElement(punto,'altitudeMode').text = '\n' + modoAltitud + '\n'
+
+
+    def addLineString(self, numero, extrude, tesela, listaCoordenadas, modoAltitud, color, ancho):
+        ET.SubElement(self.doc,'name').text = '\n' + numero + '\n'
+        pm = ET.SubElement(self.doc,'Placemark')
+        ls = ET.SubElement(pm, 'LineString')
+        ET.SubElement(ls,'extrude').text = '\n' + extrude + '\n'
+        ET.SubElement(ls,'tessellation').text = '\n' + tesela + '\n'
+        ET.SubElement(ls,'coordinates').text = '\n' + listaCoordenadas + '\n'
+        ET.SubElement(ls,'altitudeMode').text = '\n' + modoAltitud + '\n'
+
+        estilo = ET.SubElement(pm, 'Style')
+        linea = ET.SubElement(estilo, 'LineStyle')
+        ET.SubElement (linea, 'color').text = '\n' + color + '\n'
+        ET.SubElement (linea, 'width').text = '\n' + ancho + '\n'
+
+    def escribir(self, nombreArchivoKML):
+        arbol = ET.ElementTree(self.raiz)
+        arbol.write(nombreArchivoKML, encoding='utf-8', xml_declaration=True)
+
+    def ver(self):
+        """
+        Muestra el archivo KML. Se utiliza para depurar
+        """
+        print("\nElemento raiz = ", self.raiz.tag)
+
+        if self.raiz.text != None:
+            print("Contenido = "    , self.raiz.text.strip('\n')) #strip() elimina los '\n' del string
         else:
-            print("Contenido = ", hijo.text)
-        print("Atributos = ", hijo.atrib)
+            print("Contenido = "    , self.raiz.text)
+
+        print("Atributos = "    , self.raiz.attrib)
+
+        # Recorrido de los elementos del árbol
+        for hijo in self.raiz.findall('.//'): # Expresión XPath
+            print("\nElemento = " , hijo.tag)
+            if hijo.text != None:
+                print("Contenido = ", hijo.text.strip('\n')) #strip() elimina los '\n' del string
+            else:
+                print("Contenido = ", hijo.text)
+            print("Atributos = ", hijo.attrib)
+
+    def getXPath(self,archivoXML, expresionXPath):
+        try:
+            arbol = ET.parse(archivoXML)
+        except IOError:
+            print('No se encuentra el archivo ', archivoXML)
+            exit()
+        except ET.parseError:
+            print("Error procesando en el archivo XML = ", archivoXML)
+            exit()
+
+        raiz = arbol.getroot()
+
+        if('http' in str(raiz.attrib)):
+            cadena = expresionXPath.split('/')
+            aux=""
+            count=0
+            for palabra in cadena:
+                count+=1
+                aux += "{http://www.uniovi.es}"+palabra
+                if(count!=len(cadena)):
+                    aux = aux+"/"
+            expresionXPath=aux
+
+
+        elements=""
+
+        for hijo in raiz.findall(expresionXPath):
+            elements += str(hijo.attrib)
+        return elements
+
+    def cargarSalida(self):
+        coord=self.getXPath("circuitoEsquema.xml", "coordenadas/salida")
+        coord = coord[1:len(coord)-1]
+        coord = coord.split(',')
+
+        long = coord[0].split(':')[1]
+        lat = coord[1].split(':')[1]
+        alt = coord[2].split(':')[1].split("'")[1]
+
+        long = self.extraerCoordenadas(long)
+        lat = self.extraerCoordenadas(lat)
+        alt = float(alt)
+
+        #print(lat)
+        #print(long)
+        #print(alt)
+
+        long = self.convertirCoordenadas(long)
+        lat = self.convertirCoordenadas(lat)
+
+        self.addTramo("0" ,"1" ,lat ,long ,0 ,'relativeToGround')
+
+        #print(lat)
+        #print(long)
+        #print(alt)
+
+        self.coordenadas+=""+str(lat)+","+str(long)+","+str(0)+"\n"
+        self.salida=""+str(lat)+","+str(long)+","+str(0)+"\n"
+
+
+
+    def cargarTramos(self):
+        datos=self.getXPath("circuitoEsquema.xml", "coordenadas/tramo")
+        datos = re.findall(r'\{.*?\}', datos)
+
+        count = 1
+
+        for dato in datos:
+            dato=dato.split(',')
+            long = dato[1].split(":")[1]
+            lat = dato[2].split(":")[1]
+            alt = dato[3].split(":")[1].split("'")[1]
+            sec = dato[4].split(":")[1]
+
+            long = self.convertirCoordenadas(self.extraerCoordenadas(long))
+            lat = self.convertirCoordenadas(self.extraerCoordenadas(lat))
+            alt = float(alt)
+
+            self.addTramo(str(count), sec, lat, long, 0, 'relativeToGround')
+            self.coordenadas+=""+str(lat)+","+str(long)+","+str(0)+"\n"
+            count+=1
+        self.coordenadas+=self.salida.rstrip()
+
+    def extraerCoordenadas(self, coordenadas):
+        #print(coordenadas)
+        coordenadas = coordenadas[2:len(coordenadas)-1]
+        #print(coordenadas)
+        coordenadas = coordenadas.split(" ")
+        #print(coordenadas)
+        coordenadas[0] = coordenadas[0][0:len(coordenadas[0])-1]
+        coordenadas[1] = coordenadas[1][0:len(coordenadas[1])-1]
+        coordenadas[2] = coordenadas[2][0:len(coordenadas[2])-2]
+        #print(coordenadas)
+        return coordenadas
+
+    def convertirCoordenadas(self, coordenadas):
+        grados = float(coordenadas[0])
+        minutos = float(coordenadas[1])
+        segundos = float(coordenadas[2])
+
+        result = grados + (minutos/60) + (segundos/3600)
+        return result
+        #print(result)
 
 def main():
-    print(verXPath.__doc__)
-    miArchivo
+    nombreKML = "prueba.kml"
+
+    nuevoKML = Kml()
+
+    salida = nuevoKML.cargarSalida()
+
+    tramos = nuevoKML.cargarTramos()
+
+    nuevoKML.addLineString("Circuito","1","1",
+                            nuevoKML.coordenadas,'relativeToGround',
+                            '#ff0000ff', "5")
+
+    nuevoKML.ver()
+
+    nuevoKML.escribir(nombreKML)
+    print("Creado el archivo: ", nombreKML)
+
+if __name__ == "__main__":
+    main()
